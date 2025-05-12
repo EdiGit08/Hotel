173006093f83b241641ce0205a0412d94b90c39f
Edigit08 <github.edison@gmail.com>
2025-05-12 13:49:59 -0500

    Creamos una app web donde agregamos el Starup y el program, ademas modificamos el css y agregamos las paginas emergentes y ventanas, las cuales aun estan en contruccion.

---
+++ asp_presentacion/Startup.cs
@@ -0,0 +1,57 @@
+﻿using lib_presentaciones.Implementaciones;
+using lib_presentaciones.Interfaces;
+
+namespace asp_presentacion
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration? Configuration { set; get; }
+
+        public void ConfigureServices(WebApplicationBuilder builder, IServiceCollection services)
+        {
+            // Presentaciones
+            services.AddScoped<IAuditoriasPresentacion, AuditoriasPresentacion>();
+            services.AddScoped<IAuditoriasPagosPresentacion, AuditoriasPagosPresentacion>();
+            services.AddScoped<IAuditoriasReservasPresentacion, AuditoriasReservasPresentacion>();
+            services.AddScoped<IClientesPresentacion, ClientesPresentacion>();
+            services.AddScoped<IHabitacionesPresentacion, HabitacionesPresentacion>();
+            services.AddScoped<IOpinionesPresentacion, OpinionesPresentacion>();
+            services.AddScoped<IPagosPresentacion, PagosPresentacion>();
+            services.AddScoped<IPermisosPresentacion, PermisosPresentacion>();
+            services.AddScoped<IPromocionesPresentacion, PromocionesPresentacion>();
+            services.AddScoped<IRecepcionistasPresentacion, RecepcionistasPresentacion>();
+            services.AddScoped<IReservasPresentacion, ReservasPresentacion>();
+            services.AddScoped<IRoles_PermisosPresentacion, Roles_PermisosPresentacion>();
+            services.AddScoped<IRolesPresentacion, RolesPresentacion>();
+            services.AddScoped<IServicios_ReservasPresentacion, Servicios_ReservasPresentacion>();
+            services.AddScoped<IServiciosPresentacion, ServiciosPresentacion>();
+            services.AddScoped<IUsuariosPresentacion, UsuariosPresentacion>();
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddRazorPages();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+            });
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+            app.UseSession();
+            app.Run();
+        }
+    }
+}
